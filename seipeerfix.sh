b3ac979fd537ac7e407ceba06cdce6cacdc0ef76
Cameron Howe <61391064+lalilax@users.noreply.github.com>
2022-06-16 20:36:39 +0300

    Update seipeerfix.sh

--- seipeerfix.sh
+++ seipeerfix.sh
@@ -17,25 +17,24 @@ mavik="\033[36m"
 
 clear
 systemctl stop seid
-cd
-cd .sei/config
-rm -rf addrbook.json
-rm -rf config.toml
-wget https://raw.githubusercontent.com/lalilax/seipeerfix/main/addrbook.json
-wget https://raw.githubusercontent.com/lalilax/seipeerfix/main/config.toml
 clear
 echo -e "${f}\033[33m############################################################\e${f}"
 echo -e "\033[33m ${f} $mavi PEER HATASI FİX | LALILAX ${f}"
 echo -e "\033[33m############################################################\e${f}"
 su root -c "echo -e '\e[1;31m'"
 read -p "NODE ADINIZI GIRIN : " nodeadi
-cd
-cd ./sei/config
+cd /root/.sei/config
+rm -rf addrbook.json
+rm -rf config.toml
+wget https://raw.githubusercontent.com/lalilax/seipeerfix/main/addrbook.json
+wget https://raw.githubusercontent.com/lalilax/seipeerfix/main/config.toml
+clear
 sed -i -e "s/NODE_ADI/$nodeadi/" config.toml
 cd
 clear
 echo -e "\033[33m ${f} $mavi KONSOL BAŞLATILIYOR... ${f}"
 sleep 2
-journalctl -fu seid -o cat
+clear
+journalctl -u seid -f
 
 
